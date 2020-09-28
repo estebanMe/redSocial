7ac5b09d2216a69bb7c76637c7f70de8b4194c73
Esteban <victoresteban.mercado@bbva.com>
2020-09-28 17:37:37 -0300

    add saveTweet and edit user

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -18,6 +18,8 @@ func Manejadores() {
 	router.HandleFunc("/registro", middlew.ChequeoBD(routers.Registro)).Methods("POST")
 	router.HandleFunc("/login", middlew.ChequeoBD(routers.Login)).Methods("POST")
 	router.HandleFunc("/verperfil", middlew.ChequeoBD(middlew.ValidoJWT(routers.VerPerfil))).Methods("GET")
+	router.HandleFunc("/modificarPerfil", middlew.ChequeoBD(middlew.ValidoJWT(routers.ModificarPerfil))).Methods("PUT")
+	router.HandleFunc("/tweet", middlew.ChequeoBD(middlew.ValidoJWT(routers.GraboTweet))).Methods("POST")
 
 	//Si no tengo una variable de entorno PORT, asigno una con valor
 	PORT := os.Getenv("PORT")
---
+++ models/graboTweet.go
@@ -0,0 +1,10 @@
+package models
+
+import "time"
+
+//GraboTeweet es la estructura para grabar tweets
+type GraboTweet struct {
+	UserID  string    `bson:"userid" json:"userid,omitempty"`
+	Mensaje string    `bson:"mensaje" json:"mensaje,omitempty"`
+	Fecha   time.Time `bson:"fecha" json:"fecha,omitempty"`
+}
